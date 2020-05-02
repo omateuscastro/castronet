75be043fd00f617bdf17609bf6aea926cc2c78c2
unknown <mcastro.mateus@gmail.com>
2020-05-02 10:27:51 -0300

    Iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:castronet/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CastroNet',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.blue[900],
+      ),
+      home: HomePage(),
+    );
+  }
+}
